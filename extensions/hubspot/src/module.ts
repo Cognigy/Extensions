a33b14cd66adb396826e21db75e552dc7ead304f
Alexander Teusz <teusz.alexander@gmail.com>
2020-08-03 14:01:34 +0200

    added findContact node

--- extensions/hubspot/src/module.ts
+++ extensions/hubspot/src/module.ts
@@ -4,12 +4,14 @@ import { createExtension } from "@cognigy/extension-tools";
 import { findContactByEmailNode } from "./nodes/findContactByEmail";
 import { updateContactNode } from "./nodes/updateContact";
 import { createContactNode } from "./nodes/createContact";
+import { findContactNode } from "./nodes/findContact";
  
 /* import all connections */
 import { hubspotConnection } from "./connections/hubspot";
 
 export default createExtension({
 	nodes: [
+		findContactNode,
 		findContactByEmailNode,
 		updateContactNode,
 		createContactNode

6d25a0c73394f9b9784d4bb9d5eb8734f19b068f
Alexander Teusz <teusz.alexander@gmail.com>
2020-08-03 13:46:55 +0200

    added createContact node

--- extensions/hubspot/src/module.ts
+++ extensions/hubspot/src/module.ts
@@ -3,14 +3,16 @@ import { createExtension } from "@cognigy/extension-tools";
 /* import all nodes */
 import { findContactByEmailNode } from "./nodes/findContactByEmail";
 import { updateContactNode } from "./nodes/updateContact";
-
+import { createContactNode } from "./nodes/createContact";
+ 
 /* import all connections */
 import { hubspotConnection } from "./connections/hubspot";
 
 export default createExtension({
 	nodes: [
 		findContactByEmailNode,
-		updateContactNode
+		updateContactNode,
+		createContactNode
 	],
 
 	connections: [
---
+++ extensions/hubspot/src/nodes/createContact.ts
@@ -0,0 +1,167 @@
+import { createNodeDescriptor, INodeFunctionBaseParams } from "@cognigy/extension-tools";
+import Hubspot from 'hubspot';
+
+export interface ICreateContactParams extends INodeFunctionBaseParams {
+	config: {
+		data: any;
+		connection: {
+			apikey: string;
+		};
+		storeLocation: string;
+		contextKey: string;
+		inputKey: string;
+	};
+}
+
+export const createContactNode = createNodeDescriptor({
+	type: "createContact",
+	defaultLabel: "Create Contact",
+	fields: [
+		{
+			key: "connection",
+			label: "The Hubspot connection which should be used.",
+			type: "connection",
+			params: {
+				required: true,
+				connectionType: "hubspot"
+			}
+		},
+		{
+			key: "data",
+			label: "Update Contact Data",
+			type: "json",
+			defaultValue: `{
+	"properties": [
+		{
+			"property": "firstname",
+			"value": "Dave Grohl"
+		}
+	]
+}
+			`
+		},
+		{
+			key: "storeLocation",
+			type: "select",
+			label: "Where to store the result",
+			defaultValue: "input",
+			params: {
+				options: [
+					{
+						label: "Input",
+						value: "input"
+					},
+					{
+						label: "Context",
+						value: "context"
+					}
+				],
+				required: true
+			},
+		},
+		{
+			key: "inputKey",
+			type: "cognigyText",
+			label: "Input Key to store Result",
+			defaultValue: "hubspot",
+			condition: {
+				key: "storeLocation",
+				value: "input",
+			}
+		},
+		{
+			key: "contextKey",
+			type: "cognigyText",
+			label: "Context Key to store Result",
+			defaultValue: "hubspot",
+			condition: {
+				key: "storeLocation",
+				value: "context",
+			}
+		},
+	],
+	sections: [
+		{
+			key: "storage",
+			label: "Storage Option",
+			defaultCollapsed: true,
+			fields: [
+				"storeLocation",
+				"inputKey",
+				"contextKey",
+			]
+		}
+	],
+	form: [
+		{ type: "field", key: "connection" },
+		{ type: "field", key: "vid" },
+		{ type: "field", key: "data" },
+		{ type: "section", key: "storage" },
+	],
+	preview: {
+		type: "text",
+		key: "vid"
+	},
+	appearance: {
+		color: "#fa7820"
+	},
+
+	function: async ({ cognigy, config }: ICreateContactParams) => {
+		const { api } = cognigy;
+		const {
+			storeLocation,
+			contextKey,
+			inputKey,
+			data,
+			connection
+		} = config;
+		const { apikey } = connection;
+
+		try {
+			if (storeLocation === "context") api.deleteContext(contextKey);
+
+			const result = await createContact(data, apikey);
+
+			if (storeLocation === "context") api.addToContext(contextKey, result, "simple");
+			// @ts-ignore
+			else api.addToInput(inputKey, result);
+
+		} catch (err) {
+			const resultObject = {
+				result: null,
+				error: err.message
+			};
+			if (storeLocation === "context") {
+				api.addToContext(contextKey, resultObject, "simple");
+			} else {
+				// @ts-ignore
+				api.addToInput(inputKey, resultObject);
+			}
+		}
+	}
+});
+
+/**
+ * Creates a contact in Hubspot
+ * @param data The update contact data
+ * @param apiKey The apiKey to use
+ */
+async function createContact(data: any, apiKey: string): Promise<any> {
+	if (!data) return Promise.reject("No contact update data defined.");
+
+	try {
+		const hubspot = new Hubspot({ apiKey });
+		// @ts-ignore
+		if (hubspot.qs && typeof hubspot.qs === 'object') hubspot.qs.propertyMode = 'value_only';
+
+		try {
+			const response = await hubspot.contacts.create(data);
+
+			return response;
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	} catch (err) {
+		throw new Error(err.message);
+	}
+}

10a91439764cf8a6f369c4be0a693b6a79473c48
Alexander Teusz <teusz.alexander@gmail.com>
2020-08-03 13:31:57 +0200

    added updateContact node

--- extensions/hubspot/src/module.ts
+++ extensions/hubspot/src/module.ts
@@ -2,13 +2,15 @@ import { createExtension } from "@cognigy/extension-tools";
 
 /* import all nodes */
 import { findContactByEmailNode } from "./nodes/findContactByEmail";
+import { updateContactNode } from "./nodes/updateContact";
 
 /* import all connections */
 import { hubspotConnection } from "./connections/hubspot";
 
 export default createExtension({
 	nodes: [
-		findContactByEmailNode
+		findContactByEmailNode,
+		updateContactNode
 	],
 
 	connections: [
---
+++ extensions/hubspot/src/nodes/updateContact.ts
@@ -0,0 +1,185 @@
+import { createNodeDescriptor, INodeFunctionBaseParams } from "@cognigy/extension-tools";
+import Hubspot from 'hubspot';
+
+export interface IUpdateContactParams extends INodeFunctionBaseParams {
+	config: {
+		data: any;
+		vid: number;
+		connection: {
+			apikey: string;
+		};
+		storeLocation: string;
+		contextKey: string;
+		inputKey: string;
+	};
+}
+
+export const updateContactNode = createNodeDescriptor({
+	type: "updateContact",
+	defaultLabel: "Update Contact",
+	fields: [
+		{
+			key: "connection",
+			label: "The Hubspot connection which should be used.",
+			type: "connection",
+			params: {
+				required: true,
+				connectionType: "hubspot"
+			}
+		},
+		{
+			key: "vid",
+			label: "Hubspot Contact ID (VID)",
+			type: "cognigyText",
+			params: {
+				required: true
+			}
+		},
+		{
+			key: "data",
+			label: "Update Contact Data",
+			type: "json",
+			defaultValue: `
+{
+	"properties": [
+	  {
+		"property": "firstname",
+		"value": {
+			"$cs": {
+				"script": "cc.newContact.contact_firstname.value",
+				"type": "string"
+			}
+		}
+	  }
+	]
+}
+			`
+		},
+		{
+			key: "storeLocation",
+			type: "select",
+			label: "Where to store the result",
+			defaultValue: "input",
+			params: {
+				options: [
+					{
+						label: "Input",
+						value: "input"
+					},
+					{
+						label: "Context",
+						value: "context"
+					}
+				],
+				required: true
+			},
+		},
+		{
+			key: "inputKey",
+			type: "cognigyText",
+			label: "Input Key to store Result",
+			defaultValue: "hubspot",
+			condition: {
+				key: "storeLocation",
+				value: "input",
+			}
+		},
+		{
+			key: "contextKey",
+			type: "cognigyText",
+			label: "Context Key to store Result",
+			defaultValue: "hubspot",
+			condition: {
+				key: "storeLocation",
+				value: "context",
+			}
+		},
+	],
+	sections: [
+		{
+			key: "storage",
+			label: "Storage Option",
+			defaultCollapsed: true,
+			fields: [
+				"storeLocation",
+				"inputKey",
+				"contextKey",
+			]
+		}
+	],
+	form: [
+		{ type: "field", key: "connection" },
+		{ type: "field", key: "vid" },
+		{ type: "field", key: "data" },
+		{ type: "section", key: "storage" },
+	],
+	preview: {
+		type: "text",
+		key: "vid"
+	},
+	appearance: {
+		color: "#fa7820"
+	},
+
+	function: async ({ cognigy, config }: IUpdateContactParams) => {
+		const { api } = cognigy;
+		const {
+			storeLocation,
+			contextKey,
+			inputKey,
+			vid,
+			data,
+			connection
+		} = config;
+		const { apikey } = connection;
+
+		try {
+			if (storeLocation === "context") api.deleteContext(contextKey);
+
+			const result = await updateContact(vid, data, apikey);
+
+			if (storeLocation === "context") api.addToContext(contextKey, true, "simple");
+			// @ts-ignore
+			else api.addToInput(inputKey, true);
+
+		} catch (err) {
+			const resultObject = {
+				result: null,
+				error: err.message
+			};
+			if (storeLocation === "context") {
+				api.addToContext(contextKey, resultObject, "simple");
+			} else {
+				// @ts-ignore
+				api.addToInput(inputKey, resultObject);
+			}
+		}
+	}
+});
+
+/**
+ * Updates a contact in Hubspot
+ * @param vid The email hubspot contact id
+ * @param data The update contact data
+ * @param apiKey The apiKey to use
+ */
+async function updateContact(vid: number, data: any, apiKey: string): Promise<any> {
+	if (!vid) return Promise.reject("No contact id defined.");
+	if (!data) return Promise.reject("No contact update data defined.");
+
+	try {
+		const hubspot = new Hubspot({ apiKey });
+		// @ts-ignore
+		if (hubspot.qs && typeof hubspot.qs === 'object') hubspot.qs.propertyMode = 'value_only';
+
+		try {
+			const response = await hubspot.contacts.update(vid, data);
+
+			return response;
+		} catch (err) {
+			throw new Error(err.message);
+		}
+	} catch (err) {
+		throw new Error(err.message);
+	}
+}

ae596e513e1a4eb6676ff41fc54fe321fa3495a9
Alexander Teusz <teusz.alexander@gmail.com>
2020-08-18 14:18:49 +0200

    added createCompany node

--- extensions/hubspot/src/module.ts
+++ extensions/hubspot/src/module.ts
@@ -8,13 +8,15 @@ import { findContactNode } from "./nodes/findContact";
  
 /* import all connections */
 import { hubspotConnection } from "./connections/hubspot";
+import { createCompanyNode } from "./nodes/createCompany";
 
 export default createExtension({
 	nodes: [
 		findContactNode,
 		findContactByEmailNode,
 		updateContactNode,
-		createContactNode
+		createContactNode,
+		createCompanyNode
 	],
 
 	connections: [

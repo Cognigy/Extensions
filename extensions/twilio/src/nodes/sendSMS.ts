16fb6fd975e2aece9ce85a934408fbb929cb7b5a
Alexander Teusz <teusz.alexander@gmail.com>
2020-08-21 13:21:45 +0200

    connection section -> field

--- extensions/twilio/src/nodes/sendSMS.ts
+++ extensions/twilio/src/nodes/sendSMS.ts
@@ -97,14 +97,6 @@ export const sendSMSNode = createNodeDescriptor({
 	],
 	sections: [
 		{
-			key: "connectionSection",
-			label: "Connection",
-			defaultCollapsed: false,
-			fields: [
-				"connection",
-			]
-		},
-		{
 			key: "storageOption",
 			label: "Storage Option",
 			defaultCollapsed: false,
@@ -116,7 +108,7 @@ export const sendSMSNode = createNodeDescriptor({
 		}
 	],
 	form: [
-		{ type: "section", key: "connectionSection" },
+		{ type: "field", key: "connection" },
 		{ type: "field", key: "from" },
 		{ type: "field", key: "to" },
 		{ type: "field", key: "body" },

2038ddbe394e455a21b34bbc30731dc7fddcb127
Christoph Bullmann <c.bullmann@cognigy.com>
2020-08-28 09:16:46 +0200

    removed unnecessary sendMessage

--- extensions/voice-gateway/src/module.ts
+++ extensions/voice-gateway/src/module.ts
@@ -2,13 +2,11 @@ import { createExtension } from "@cognigy/extension-tools";
 
 import { handoverNode } from "./nodes/handover";
 import { hangupNode } from "./nodes/hangup";
-import { sendMessageNode } from "./nodes/sendMessage";
 import { setSessionParamsNode } from "./nodes/setSessionParams";
 
 
 export default createExtension({
 	nodes: [
-		sendMessageNode,
 		handoverNode,
 		hangupNode,
 		setSessionParamsNode

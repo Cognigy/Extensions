19314f662b1fdb7ea63d45a2db047efffda81a26
Alexander Teusz <teusz.alexander@gmail.com>
2020-09-08 13:10:20 +0200

    added livechat nodes

--- extensions/salesforce/src/module.ts
+++ extensions/salesforce/src/module.ts
@@ -4,15 +4,25 @@ import { sqlQueryNode } from "./nodes/sqlQuery";
 import { createEntityNode } from "./nodes/createEntity";
 import { salesforceConnection } from "./connections/salesforce";
 import { getEntityNode } from "./nodes/getEntity";
+import { checkLiveAgentAvailabilityNode } from "./nodes/live-chat/checkLiveAgentAvailability";
+import { sendMessageToLiveAgentNode } from "./nodes/live-chat/sendMessageToLiveAgent";
+import { startLiveChatNode } from "./nodes/live-chat/startLiveChat";
+import { stopLiveChatNode } from "./nodes/live-chat/stopLiveChat";
+import { livechatConnection } from "./connections/liveChat";
 
 export default createExtension({
 	nodes: [
 		sqlQueryNode,
 		createEntityNode,
-		getEntityNode
+		getEntityNode,
+		checkLiveAgentAvailabilityNode,
+		sendMessageToLiveAgentNode,
+		startLiveChatNode,
+		stopLiveChatNode
 	],
 
 	connections: [
-		salesforceConnection
+		salesforceConnection,
+		livechatConnection
 	]
 });

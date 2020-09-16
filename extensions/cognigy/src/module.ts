78748a24e5623883fd3c7aafdb7030df1de68351
Alexander Teusz <teusz.alexander@gmail.com>
2020-09-16 15:26:33 +0200

    added disambiguation node

--- extensions/cognigy/src/module.ts
+++ extensions/cognigy/src/module.ts
@@ -4,12 +4,13 @@ import { sendEmailWithAttachmentNode } from "./nodes/sendEmailWithAttachment";
 import { smtpConnection } from "./connections/smtpConnection";
 import { getConversationNode } from "./nodes/getConversation";
 import { cognigyApiConnection } from "./connections/cognigyApiConnection";
-
+import { intentDisambiguationNode } from "./nodes/intentDisambiguation";
 
 export default createExtension({
 	nodes: [
 		sendEmailWithAttachmentNode,
-		getConversationNode
+		getConversationNode,
+		intentDisambiguationNode
 	],
 
 	connections: [

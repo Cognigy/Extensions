b2b3f90c7c85ceb808b85f2830e5f5481587823e
Alexander Teusz <teusz.alexander@gmail.com>
2021-02-01 16:23:48 +0100

    wip

--- extensions/ringcentral-engage/src/module.ts
+++ extensions/ringcentral-engage/src/module.ts
@@ -1,10 +1,16 @@
 import { createExtension } from "@cognigy/extension-tools";
 
 import { handoverNode } from "./nodes/handover";
+import { sendRichLinkNode } from "./nodes/sendRichLink";
+import { sendSelectNode } from "./nodes/sendSelect";
+import { sendTimePickerNode } from "./nodes/sendTimePicker";
 
 export default createExtension({
 	nodes: [
-		handoverNode
+		handoverNode,
+		sendSelectNode,
+		sendRichLinkNode,
+		sendTimePickerNode
 	],
 
 	connections: []
---
+++ extensions/ringcentral-engage/src/nodes/sendTimePicker.ts
@@ -0,0 +1,100 @@
+import { createNodeDescriptor, INodeFunctionBaseParams } from "@cognigy/extension-tools";
+
+
+export interface ISendTimePickerParams extends INodeFunctionBaseParams {
+	config: {
+        attachmentId: string;
+        title: string;
+        subtitle: string;
+        url: string;
+        urlText: string;
+	};
+}
+export const sendTimePickerNode = createNodeDescriptor({
+	type: "sendTimePickerRingCentral",
+	defaultLabel: "Send Time Picker",
+	fields: [
+		{
+            key: "attachmentId",
+            type: "cognigyText",
+            label: "Attachment ID",
+            description: "The RingCentral Attachment ID for the image that should be used"
+        },
+        {
+            key: "title",
+            type: "cognigyText",
+            label: "Title",
+            params: {
+                required: true
+            }
+        },
+        {
+            key: "subtitle",
+            type: "cognigyText",
+            label: "Subtitle",
+            params: {
+                required: true
+            }
+        },
+        {
+            key: "url",
+            type: "cognigyText",
+            label: "URL",
+            params: {
+                required: true
+            }
+        },
+        {
+            key: "urlText",
+            type: "cognigyText",
+            label: "URL Text",
+            params: {
+                required: true
+            }
+        }
+    ],
+    sections: [
+        {
+            key: "attachment",
+            label: "Attachment",
+            defaultCollapsed: true,
+            fields: [
+                "attachmentId"
+            ]
+        }
+    ],
+	form: [
+		{ type: "field", key: "title" },
+        { type: "field", key: "subtitle" },
+        { type: "field", key: "url" },
+        { type: "field", key: "urlText" },
+        { type: "section", key: "attachment" }
+	],
+	appearance: {
+		color: "#FF8800"
+	},
+	function: async ({ cognigy, config }: ISendTimePickerParams) => {
+		const { api } = cognigy;
+		const { attachmentId, title, subtitle, url, urlText } = config;
+
+		api.say('', {
+			_cognigy: {
+				_ringCentralEngage: {
+					json: {
+						command: "structured-content",
+						structuredContent: {
+                            type: "rich_link",
+                            attachment_id: attachmentId,
+                            attachment_fallback_id: attachmentId,
+                            title,
+                            subtitle,
+                            url,
+                            url_fallback: url,
+                            url_text: urlText
+						}
+					}
+				}
+			}
+		});
+	}
+});

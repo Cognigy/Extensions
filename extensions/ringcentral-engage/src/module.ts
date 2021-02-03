26ed861edf8c66630aca60fbcd6080972ac33f57
Rémi Bauby <33779889+r2m1@users.noreply.github.com>
2021-02-03 18:23:49 +0100

    Update module.ts
    Update module with one new node (Close Intervention).
    The node itself needs to be reviewed to make the API call possible from the github repo.

--- extensions/ringcentral-engage/src/module.ts
+++ extensions/ringcentral-engage/src/module.ts
@@ -1,6 +1,7 @@
 import { createExtension } from "@cognigy/extension-tools";
 
 import { handoverNode } from "./nodes/handover";
+import { closeInterventionNode } from "./nodes/closeIntervention";
 import { sendApplePaykNode } from "./nodes/sendApplePay";
 import { sendRichLinkNode } from "./nodes/sendRichLink";
 import { sendSelectNode } from "./nodes/sendSelect";
@@ -9,6 +10,7 @@ import { sendTimePickerNode } from "./nodes/sendTimePicker";
 export default createExtension({
 	nodes: [
 		handoverNode,
+		closeInterventionNode,
 		sendSelectNode,
 		sendRichLinkNode,
 		sendTimePickerNode,

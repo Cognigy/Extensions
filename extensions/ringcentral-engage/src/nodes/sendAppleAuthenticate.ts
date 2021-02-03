ecd12a2481a0b73277f8e7feb6651bfdf9399e6e
Rémi Bauby <33779889+r2m1@users.noreply.github.com>
2021-02-03 19:03:22 +0100

    Initial Apple Authentication node
    Used the Apple pay node model to create this one that should be fine.
    Full documentation on what is requested is available here: https://developers.ringcentral.com/engage/digital/guide/interactions/structured-messages/apple-auth

---
+++ extensions/ringcentral-engage/src/nodes/sendAppleAuthenticate.ts
@@ -0,0 +1,66 @@
+import { createNodeDescriptor, INodeFunctionBaseParams } from "@cognigy/extension-tools";
+
+
+export interface ISendAppleAuthParams extends INodeFunctionBaseParams {
+	config: {
+        attachmentId: string;
+        
+        merchant_session: object;
+        supported_networks: string[];
+	};
+}
+export const sendAppleAuthNode = createNodeDescriptor({
+	type: "sendAppleAuthRingCentral",
+	defaultLabel: "Send Apple Authenticate",
+	fields: [
+		{
+            key: "attachmentId",
+            type: "cognigyText",
+            label: "Attachment ID",
+            description: "The RingCentral Attachment ID for the image that should be used"
+        },
+        {
+            key: "response_body",
+            type: "cognigyText",
+            label: "Text",
+            params: {
+                required: false
+            }
+        },
+    sections: [
+        {
+            key: "attachment",
+            label: "Attachment",
+            defaultCollapsed: true,
+            fields: [
+                "attachmentId"
+            ]
+        }
+    ],
+	form: [
+        { type: "field", key: "Text" }
+        { type: "section", key: "attachment" }
+	],
+	appearance: {
+		color: "#FF8800"
+	},
+	function: async ({ cognigy, config }: ISendAppleAuthParams) => {
+		const { api } = cognigy;
+		const { attachmentId, response_body} = config;
+
+        api.say('', {
+			_cognigy: {
+				_ringCentralEngage: {
+					json: {
+						command: "structured-content",
+						structuredContent: {
+                            type: "authenticate",
+                            attachment_id: attachmentId,
+                            response_body: response_body
+						}
+					}
+				}
+			}
+		});
+	}
+});

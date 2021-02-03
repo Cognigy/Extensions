4a4c762eb147639c34378aa6338c5f1730548afe
Rémi Bauby <33779889+r2m1@users.noreply.github.com>
2021-02-03 19:13:02 +0100

    Update module.ts

--- extensions/ringcentral-engage/src/module.ts
+++ extensions/ringcentral-engage/src/module.ts
@@ -3,6 +3,7 @@ import { createExtension } from "@cognigy/extension-tools";
 import { handoverNode } from "./nodes/handover";
 import { closeInterventionNode } from "./nodes/closeIntervention";
 import { sendApplePaykNode } from "./nodes/sendApplePay";
+import { sendAppleAuthNode } from "./nodes/sendAppleAuthentication";
 import { sendRichLinkNode } from "./nodes/sendRichLink";
 import { sendSelectNode } from "./nodes/sendSelect";
 import { sendTimePickerNode } from "./nodes/sendTimePicker";
@@ -15,6 +16,7 @@ export default createExtension({
 		sendRichLinkNode,
 		sendTimePickerNode,
 		sendApplePaykNode
+		SendAppleAuthenticateNode
 	],
 
 	connections: []

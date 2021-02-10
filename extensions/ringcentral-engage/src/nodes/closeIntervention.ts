91c419d38f222c5e2f2ca080cf9089fb7035a1cb
Rémi Bauby <33779889+r2m1@users.noreply.github.com>
2021-02-10 09:14:58 +0100

    Update the fuction to call API -- Work in progress
    Need a check from Cognigy to:
    - set the connection right using information from the endpoint configuration (domain, access_token)
    - access the context information to set the intervention_id parameter right
    - general check and test

--- extensions/ringcentral-engage/src/nodes/closeIntervention.ts
+++ extensions/ringcentral-engage/src/nodes/closeIntervention.ts
@@ -15,20 +15,58 @@ export const closeInterventionNode = createNodeDescriptor({
 	appearance: {
 		color: "#FF8800"
     },
-    
+	
+	
 	function: async ({ cognigy, config }: ICloseInterventionParams) => {
-		const { api, input} = cognigy;
+		const { api } = cognigy;
+		const { space, connection, storeLocation, contextKey, inputKey } = config;
+		const { domain, access_token } = connection;
+
+		try {
+			const response = await axios({
+				method: 'put',
+				url: `${domain}/interventions/${intervention_id}/close',
+				headers: {
+					'Authorization': 'Bearer ${access_token}'
+				},
+			});
+
+			if (storeLocation === "context") {
+				api.addToContext(contextKey, results, "simple");
+			} else {
+				// @ts-ignore
+				api.addToInput(inputKey, results);
+			}
+		} catch (error) {
+			if (storeLocation === "context") {
+				api.addToContext(contextKey, error.message, "simple");
+			} else {
+				// @ts-ignore
+				api.addToInput(inputKey, error.message);
+			}
+		}
+	}
+});
+	
+		
+	
+	
+	
+	
+    
+	//function: async ({ cognigy, config }: ICloseInterventionParams) => {
+	//	const { api, input} = cognigy;
 		// Send RingCentral Engage Command
 
         // How can I directly call ED API and use Cognigy context data in that call for 'API URL from the endpoint set up' and for 'intervention_id' as Cognigy automatically creates the intervention on the first reply ?
-		api.say('', {
-			_cognigy: {
-				_ringCentralEngage: {
-					json: { 
-						command: "curl --location --request PUT '<context.API_URL>/interventions/<input.intervention_id>/close'",
-					}
-				}
-			}
-		});
-	}
-});
+	//	api.say('', {
+	//		_cognigy: {
+	//			_ringCentralEngage: {
+	//				json: { 
+	//					command: "curl --location --request PUT '<context.API_URL>/interventions/<input.intervention_id>/close'",
+	//				}
+	//			}
+	//		}
+	//	});
+//	}
+//});

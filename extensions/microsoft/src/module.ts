60f8a944053b4721ec4f031f931272ee562314de
Alexander Teusz <teusz.alexander@gmail.com>
2021-02-24 17:56:34 +0100

    added sendChannelMessage node for microsoft teams

--- extensions/microsoft/src/module.ts
+++ extensions/microsoft/src/module.ts
@@ -5,6 +5,7 @@ import { getAuthenticationTokenNode } from "./nodes/getAuthenticationToken";
 import { getEventsFromCalendarNode } from "./nodes/getEventsFromCalendar";
 import { scheduleMeetingNode } from "./nodes/scheduleMeeting";
 import { getUserDetailsNode } from "./nodes/getUserDetails";
+import { sendChannelMessageNode } from "./nodes/microsoft-teams/sendChannelMessage";
 
 
 export default createExtension({
@@ -13,7 +14,8 @@ export default createExtension({
 		getAuthenticationTokenNode,
 		getEventsFromCalendarNode,
 		scheduleMeetingNode,
-		getUserDetailsNode
+		getUserDetailsNode,
+		sendChannelMessageNode
 	],
 	connections: [
 		loginConnection

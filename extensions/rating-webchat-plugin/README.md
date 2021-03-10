28d020147f9b7fb8d99a6034febed3e18b6ad760
Anton Trukhanyonok <antonxt@gmail.com>
2021-03-11 00:34:56 +0100

    Finalized the rating card extension for code review

--- extensions/rating-webchat-plugin/README.md
+++ extensions/rating-webchat-plugin/README.md
@@ -1,3 +1,22 @@
-# Rating Webchat Plugin companion
+# Rating Card Webchat Plugin companion extension
 
-A companion node to [Rating Webchat Plugin](https://github.com/Cognigy/WebchatPlugins/tree/master/plugins/rating). The node shows the rating card with the properties set by the user
+A companion extension to [Rating Webchat Plugin](https://github.com/Cognigy/WebchatPlugins/tree/master/plugins/rating). It adds a Show Rating Card node that shows a rating card with the properties set by the user. The webchat must load the Rating plugin to render this card, otherwise it will not show anything.
+
+The Show Rating Card node simply sends a message, which is equivalent to running the following in a Code node:
+
+```json
+api.say('', {
+  "_plugin": {
+    "type": "rating",
+    "title": "Rating message example", // Is required
+    "initialRating": 1, // 1, 2, ..., 10
+    "size": "large", // "small", "medium", "large"
+    "variant": "emoji", // "star", "heart", "emoji" 
+    "maxRatingValue": 2, // 2, 3, ..., 10
+    "precision": 1, // 0.5, 1
+    "rateButtonText": "Send Rating",  // Is required
+    "label": "Rating submitted", // Can be empty
+    "payload": "stars" // Can be empty
+  }
+});
+```
--- extensions/rating-webchat-plugin/src/nodes/rating.ts
+++ extensions/rating-webchat-plugin/src/nodes/rating.ts
@@ -1,138 +1,160 @@
 import { createNodeDescriptor, INodeFunctionBaseParams } from "@cognigy/extension-tools";
 
 export interface IRatingCardParams extends INodeFunctionBaseParams {
-	config: {
-		title: string;
-		initialRating: number;
-		maxRatingValue: number;
-		precision: number;
-		variant: string;
-		size: string;
-		rateButtonText: string;
-	};
+  config: {
+    initialRating: number;
+    title: string;
+    maxRatingValueHeart: number;
+    maxRatingValueStar: number;
+    precisionHeart: number;
+    precisionStar: number;
+    variant: string;
+    size: string;
+    rateButtonText: string;
+    label: string;
+    payload: string;
+  };
 }
-/*
-api.say('', {
-  "_plugin": {
-    "type": "rating",
-    "title": "How did it go? Please rate us",
-    "initialRating": 0,
-    "size": "large", // 'small', 'medium', 'large'
-    "variant": "star", // 'star', 'heart', 'emoji'
-    "maxRatingValue": 5,
-    "precision": 0.5, // '1' or '0.5' for rating values like 3.5
-    "rateButtonText": "Send Rating"
-  }
-});
-    "initialRating": 2,
-    "size": "medium", // 'small', 'medium', 'large'
-    "variant": "star", // 'star', 'heart', 'emoji'
-    "maxRatingValue": 5,
-    "precision": 1, // '1' or '0.5' for rating values like 3.5
-    "rateButtonText": "Send Rating"
-*/
 
 export const rating = createNodeDescriptor({
-	type: "ratingCard",
-	defaultLabel: "Rate",
-	fields: [
-		{
-			key: "title",
-			label: "Title",
-			type: "cognigyText",
-			defaultValue: "Please rate us",
-			params: {
-				required: true
-			}
-		},
-		{
-			key: "initialRating",
-			label: "Initial Rating",
-			type: "slider",
-			params: {
-				min: 0,
-				max: 5,
-				step: 1
-			}
-		},
-		{
-			key: "maxRatingValue",
-			label: "Maximal Rating",
-			type: "slider",
-			defaultValue: 5,
-			params: {
-				min: 0,
-				max: 5,
-				step: 1,
-				required: true
-			}
-		},
-		{
-			key: "precision",
-			label: "Precision",
-			type: "slider",
-			defaultValue: "1",
-			params: {
-				min: 0.5,
-				max: 1,
-				step: 0.5,
-				required: true
-			}
-		},
-		{
-			key: "size",
-			label: "Size",
-			type: "select",
-			defaultValue: "medium",
-			params: {
-				options: [
-					{ value: "small", label: "Small"},
-					{ value: "medium", label: "Medium"},
-					{ value: "large", label: "Large"}
-				]
-			}
-		},
-		{
-			key: "variant",
-			label: "Variant",
-			type: "select",
-			defaultValue: "star",
-			params: {
-				options: [
-					{ value: "star", label: "Star"},
-					{ value: "heart", label: "Heart"},
-					{ value: "emoji", label: "Emoji"}
-				]
-			}
-		},
-		{
-			key: "rateButtonText",
-			label: "Rate Button Text",
-			type: "cognigyText",
-			defaultValue: "Send Rating",
-			params: {
-				required: true
-			}
-		}
-	],
-	preview: {
-		type: "text",
-		key: "text"
-	},
-	function: async ({ cognigy, config }: IRatingCardParams) => {
-		const { api } = cognigy;
-		// const { title } = config;
-		api.say('', {
-			"_plugin": {
-				"type": "rating",
-				"title": config.title,
-				"initialRating": config.initialRating,
-				"size": config.size,
-				"variant": config.variant,
-				"maxRatingValue": config.maxRatingValue,
-				"precision": config.precision,
-				"rateButtonText": config.rateButtonText
-			}
-		});
-	}
+  type: "ratingCard",
+  defaultLabel: "Show Rating Card",
+  fields: [
+    {
+      key: "title",
+      type: "cognigyText",
+      label: "Rating card title",
+      defaultValue: "Please give us your rating",
+      params: { required: true }
+    },
+    {
+      key: "variant",
+      type: "select",
+      label: "Variant",
+      defaultValue: "star",
+      params: {
+        options: [
+          { value: "star", label: "Star" },
+          { value: "heart", label: "Heart" },
+          { value: "emoji", label: "Emoji" }
+        ]
+      }
+    },
+    {
+      key: "initialRating",
+      label: "Initial rating when card shows up",
+      type: "slider",
+      params: { min: 0, max: 10, step: 1 }
+    },
+    { // TODO: remove these duplicates when advanced conditions are available
+      key: "maxRatingValueStar",
+      label: "Maximal Rating",
+      type: "slider",
+      defaultValue: 5,
+      params: { min: 2, max: 10, step: 1 },
+      condition: { key: "variant", value: "star" }
+    },
+    { // TODO: remove these duplicates when advanced conditions are available
+      key: "maxRatingValueHeart",
+      label: "Maximal Rating",
+      type: "slider",
+      defaultValue: 5,
+      params: { min: 0, max: 10, step: 1 },
+      condition: { key: "variant", value: "heart" }
+    },
+    { // TODO: remove these duplicates when advanced conditions are available
+      key: "precisionStar",
+      label: "Rating precision",
+      type: "slider",
+      defaultValue: "1",
+      params: { min: 0.5, max: 1, step: 0.5 },
+      condition: { key: "variant", value: "star" }
+    },
+    { // TODO: remove these duplicates when advanced conditions are available
+      key: "precisionHeart",
+      label: "Rating precision",
+      type: "slider",
+      defaultValue: "1",
+      params: { min: 0.5, max: 1, step: 0.5 },
+      condition: { key: "variant", value: "heart" }
+    },
+    {
+      key: "size",
+      label: "Size of rating icons",
+      type: "select",
+      defaultValue: "medium",
+      params: {
+        options: [
+          { value: "small", label: "Small" },
+          { value: "medium", label: "Medium" },
+          { value: "large", label: "Large" }
+        ]
+      }
+    },
+    {
+      key: "rateButtonText",
+      label: "Text on button that submits rating",
+      type: "cognigyText",
+      defaultValue: "Submit Rating",
+      params: { required: true }
+    },
+    {
+      key: "label",
+      label: "Message bubble from user after rating submitted",
+      type: "cognigyText"
+    },
+    {
+      key: "payload",
+      label: "Text after rating value in user message",
+      type: "cognigyText"
+    }
+  ],
+  preview: { type: "text", key: "text" },
+  function: async ({ cognigy, config }: IRatingCardParams) => {
+    const { api } = cognigy;
+    const {
+      title,
+      initialRating,
+      maxRatingValueHeart,
+      maxRatingValueStar,
+      precisionHeart,
+      precisionStar,
+      variant,
+      size,
+      rateButtonText,
+      label,
+      payload
+    } = config;
+
+    // TODO: refactor this when advanced conditions are available
+    let maxRatingValue = 5;
+    let precision = 1;
+    switch (variant) {
+      case 'star': {
+        maxRatingValue = maxRatingValueStar;
+        precision = precisionStar;
+        break;
+      }
+      case 'heart': {
+        maxRatingValue = maxRatingValueHeart;
+        precision = precisionHeart;
+        break;
+      }
+    }
+
+    api.say('', {
+      '_plugin': {
+        type: 'rating',
+        title: title,  // "Please rate your experience"
+        initialRating: initialRating, // 0, 1, 2, ...
+        size: size,  // "small", "medium", "large"
+        variant: variant,  // "star", "heart", "emoji"
+        maxRatingValue: maxRatingValue, // 2, 3, 4, ...
+        precision: precision, // 0.5, 1
+        rateButtonText: rateButtonText, // "Submit Rating"
+        label: label || '',  // Here is my rating"
+        payload: payload || ''  // "stars"
+      }
+    });
+  }
 });

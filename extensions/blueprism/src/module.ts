50359f24b0d19ac4db04b347407edd20b3eb72fb
Anton Trukhanyonok <antonxt@gmail.com>
2021-05-11 15:46:22 +0200

    Removed blueprism code which got into this branch by mistake

--- extensions/blueprism/src/module.ts
+++
@@ -1,8 +0,0 @@
-import { createExtension } from "@cognigy/extension-tools";
-import { startBlueprismProcess } from "./nodes/startBlueprismProcess";
-import { blueprismConnection as blueprismConnection } from "./connections/blueprismConnection";
-
-export default createExtension({
-  nodes: [startBlueprismProcess],
-  connections: [blueprismConnection]
-});

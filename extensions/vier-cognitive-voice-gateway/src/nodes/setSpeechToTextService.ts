ab0a42aec206da2579fcd8e65742834b95156702
Ergin Denis <denis.ergin@vier.ai>
2022-06-21 09:18:49 +0200

    Implement a fallback for switching STT

--- extensions/vier-cognitive-voice-gateway/src/nodes/setSpeechToTextService.ts
+++ extensions/vier-cognitive-voice-gateway/src/nodes/setSpeechToTextService.ts
@@ -1,13 +1,41 @@
 import { createNodeDescriptor, INodeFunctionBaseParams } from '@cognigy/extension-tools/build';
+import { INodeField, INodeFieldTranslations } from '@cognigy/extension-tools/build/interfaces/descriptor';
 import { t } from '../helpers/translations';
 
 export interface ISetSpeechToTextServiceParams extends INodeFunctionBaseParams {
   config: {
     language?: string,
-    transcribers: string,
-    profileToken: string,
+    transcriber: string,
+    profileToken?: string,
+    transcriberFallback?: string,
+    profileTokenFallback?: string,
   };
 }
+
+const generateTranscriberSelect = (key: string, required: boolean, label: INodeFieldTranslations): INodeField => ({
+  type: 'select',
+  key,
+  label,
+  params: {
+    required,
+    options: [
+      { value: 'GOOGLE', label: 'Google' },
+      { value: 'MICROSOFT', label: 'Microsoft' },
+      { value: 'IBM', label: 'IBM' },
+    ]
+  }
+});
+
+const generateProfileTokenInput = (key: string, label: INodeFieldTranslations, description: INodeFieldTranslations): INodeField => ({
+  type: 'text',
+  key,
+  label,
+  description,
+  params: {
+    required: false,
+    placeholder: '',
+  }
+});
 
 export const setSpeechtoTextServiceNode = createNodeDescriptor({
   type: 'speechToText',
@@ -16,31 +44,11 @@ export const setSpeechtoTextServiceNode = createNodeDescriptor({
   appearance: {
     color: 'blue'
   },
- 
   fields: [
-    {
-      type: 'select',
-      key: 'transcribers',
-      label: t('speechToText.inputServiceLabel'),
-      params: {
-        required: true,
-        options: [
-          { value: 'GOOGLE', label: 'Google' },
-          { value: 'MICROSOFT', label: 'Microsoft' },
-          { value: 'IBM', label: 'IBM' },
-        ]
-      }
-    },
-    {
-      type: 'text',
-      key: 'profileToken',
-      label: t('speechToText.inputProfileTokenLabel'),
-      description: t('speechToText.inputProfileTokenDescription'),
-      params: {
-        required: false,
-        placeholder: '',
-      }
-    },
+    generateTranscriberSelect('transcriber', true, t('speechToText.inputServiceLabel')),
+    generateProfileTokenInput('profileToken', t('speechToText.inputProfileTokenLabel'), t('speechToText.inputProfileTokenDescription')),
+    generateTranscriberSelect('transcriberFallback', false, t('speechToText.inputServiceFallbackLabel')),
+    generateProfileTokenInput('profileTokenFallback', t('speechToText.inputProfileTokenFallbackLabel'), t('speechToText.inputProfileTokenFallbackDescription')),
     {
       type: 'select',
       key: 'language',
@@ -57,39 +65,49 @@ export const setSpeechtoTextServiceNode = createNodeDescriptor({
 
   sections: [
     {
-      key: 'selectSTT',
-      fields: ['transcribers', 'profileToken'],
+      key: 'selectMainSTT',
+      fields: ['transcriber', 'profileToken'],
       label:  t('speechToText.sectionSelectSTTLabel'),
       defaultCollapsed: false,
     },
     {
-      key: 'selectLanguage',
-      fields: ['language'],
-      label:  t('speechToText.sectionSelectLanguageLabel'),
+      key: 'selectFallbackSTT',
+      fields: ['transcriberFallback', 'profileTokenFallback'],
+      label:  t('speechToText.sectionFallback'),
       defaultCollapsed: true,
     },
   ],
   form: [
     {
-      key: 'selectSTT',
+      key: 'language',
+      type: 'field',
+    },
+    {
+      key: 'selectMainSTT',
       type: 'section',
     },
     {
-      key: 'selectLanguage',
+      key: 'selectFallbackSTT',
       type: 'section',
-    },
+    }
   ],
   function: async ({ cognigy, config }: ISetSpeechToTextServiceParams) => {
     const { api } = cognigy;
     const transcriber = [];
 
     if (config.profileToken) {
-      transcriber.push(`${config.transcribers}-${config.profileToken}`);
+      transcriber.push(`${config.transcriber}-${config.profileToken}`);
     } else {
-        transcriber.push(config.transcribers);
+      transcriber.push(config.transcriber);
     }
-  
-    api.say("" , {
+
+    if (config.transcriberFallback && config.profileTokenFallback) {
+      transcriber.push(`${config.transcriberFallback}-${config.profileTokenFallback}`);
+    } else if (config.transcriberFallback) {
+      transcriber.push(`${config.transcriberFallback}`);
+    }
+
+    api.say('' , {
       status: 'transcription-switch',
       language: config.language,
       transcribers: transcriber,

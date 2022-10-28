eb3f06bad8867b29b40e215201dc78ab53d50d31
Alex Teusz <a.teusz@cognigy.com>
2022-10-28 14:04:50 +0200

    initially added checkLiveAgentAvailability

--- extensions/liveperson/src/module.ts
+++ extensions/liveperson/src/module.ts
@@ -1,9 +1,18 @@
 import { createExtension } from "@cognigy/extension-tools";
+import { livepersonConnection } from "./connections/livepersonConnection";
+import { checkLiveAgentAvailabilityNode, onAgentAvailable, onNoAgentAvailable } from "./nodes/checkLiveAgentAvailability";
 import { handovertoAgentNode } from "./nodes/handoverToAgent";
 
 export default createExtension({
 	nodes: [
-		handovertoAgentNode
+		handovertoAgentNode,
+		checkLiveAgentAvailabilityNode,
+		onAgentAvailable,
+		onNoAgentAvailable
+	],
+
+	connections: [
+		livepersonConnection
 	],
 
 	options: {

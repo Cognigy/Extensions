87e03d5c952215e5e5e6de7978d1148eabd22e11
Alex Teusz <teusz.alexander@gmail.com>
2024-02-23 09:22:14 +0100

    Revert "8x8 Send SMS Node"

--- extensions/8x8/src/nodes/SMS/index.ts
+++
@@ -1,8 +0,0 @@
-import type { INodeDescriptor } from "@cognigy/extension-tools";
-import { onErrorSMS, onSuccessSMS, sendSMSNode } from "./sendSMS";
-
-export const getSMSNodes = (): INodeDescriptor[] => [
-  sendSMSNode,
-  onSuccessSMS,
-  onErrorSMS,
-];

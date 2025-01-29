895caaf8bae776bc9a703902e280906f3453ae4d
Baz125 <barrymc86@hotmail.com>
2025-01-29 11:27:22 +0100

    Add subdomain to api token connection, Dynamically select subdomain based on connection type in create ticket node

--- extensions/zendesk/src/connections/zendeskAPITokenConnection.ts
+++ extensions/zendesk/src/connections/zendeskAPITokenConnection.ts
@@ -1,10 +1,11 @@
 import { IConnectionSchema } from "@cognigy/extension-tools";
 
 export const zendeskAPITokenConnection: IConnectionSchema = {
-	type: "zendesk-api-token",
-	label: "Zendesk API Token",
-	fields: [
-		{ fieldName: "emailAddress" },
-		{ fieldName: "apiToken" }
-	]
+  type: "zendesk-api-token",
+  label: "Zendesk API Token",
+  fields: [
+    { fieldName: "emailAddress" },
+    { fieldName: "apiToken" },
+    { fieldName: "subdomain" },
+  ],
 };

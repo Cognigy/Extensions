697c4998f8066bd397a79c24e049d117684aa220
f.asad <f.asad@cognigy.com>
2025-09-02 11:01:32 +0200

    Update eslint config

--- extensions/confluence/eslint.config.mjs
+++ extensions/confluence/eslint.config.mjs
@@ -8,7 +8,7 @@ export default tseslint.config(
   tseslint.configs.recommended,
   {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
     }
   },
   {
